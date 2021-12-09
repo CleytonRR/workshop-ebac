2ca2c561a8a432931e088e378346d760c12af123
Cleyton Furtado <cleytonrodrigues69@live.com>
2021-12-09 14:34:14 -0300

    Added list method

--- packages/catalog-service/src/repository/index.js
+++ packages/catalog-service/src/repository/index.js
@@ -3,5 +3,9 @@ const Movie = require("../model/Movie")
 module.exports = {
   async create(data) {
     await Movie.create({...data})
+  },
+
+  async list() {
+    return await Movie.find()
   }
 }

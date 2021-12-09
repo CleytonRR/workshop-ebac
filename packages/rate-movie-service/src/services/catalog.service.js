73ab7f83458718e864ed6fe8b616267467cdded0
Cleyton Furtado <cleytonrodrigues69@live.com>
2021-12-09 15:10:55 -0300

    Rename service

--- packages/rate-movie-service/src/services/catalog.service.js
+++ packages/rate-movie-service/src/services/catalog.service.js
@@ -1,14 +1,14 @@
 const repository = require("../repository")
 
 module.exports = {
-  name: "catalog-service",
+  name: "rate-movie-service",
   version: 1,
   dependencies: [
     "v1.catalog-service"
   ],
   actions: {
    create: {
-    async rateMovie(ctx) {
+    async handler(ctx) {
       const idMovie = ctx.params.idMovie;
       const rate = ctx.params.rate;
       const idUser = ctx.params.idUser;

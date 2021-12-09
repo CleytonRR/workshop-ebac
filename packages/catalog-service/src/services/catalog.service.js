40a76e0bc6f3658b00b03fd00f4e1123f53d0ec3
Cleyton Furtado <cleytonrodrigues69@live.com>
2021-12-09 14:34:43 -0300

    Added create and list services'

--- packages/catalog-service/src/services/catalog.service.js
+++ packages/catalog-service/src/services/catalog.service.js
@@ -1,4 +1,28 @@
+const repository = require("../repository")
+
 module.exports = {
   name: "catalog-service",
-  version: 1
+  version: 1,
+
+  actions: {
+   create: {
+     async handler(ctx) {
+       const {name, sinopse} = ctx.params
+
+       if(!name || !sinopse) {
+         throw "Name e sinopse são obrigatórios"
+       }
+
+       repository.create({name, sinopse})
+     }
+   },
+
+   listAll: {
+     async handler() {
+       const allMovies = await repository.list()
+
+       return allMovies
+     }
+   }
+  }
 }
